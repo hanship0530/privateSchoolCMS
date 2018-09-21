104a2addf82dc0f6d0fdc8759845ea726d232e94
hangeulBae <dxdiag20@gmail.com>
2018-09-22 01:35:14 +0900

    dashboard: add home function

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Note 
 
 # Register your models here.
+class NoteAdmin(admin.ModelAdmin):
+	list_display = ('number', 'text')
+
+admin.site.register(Note, NoteAdmin)
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Note(models.Model):
+	number = models.AutoField(primary_key=True, verbose_name="번호")	
+	text = models.TextField(default=' ', verbose_name="내용")
+	#writer = CharField()
