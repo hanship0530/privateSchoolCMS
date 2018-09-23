75872c2dc00eaab26c48aeb6f0fd997530f84b26
hangeulBae <dxdiag20@gmail.com>
2018-09-23 18:32:17 +0900

    add payment's state to database

--- payments/models.py
+++ payments/models.py
@@ -5,13 +5,18 @@ from django.utils import timezone
 # Create your models here.
 class Payment(models.Model):
 	PAYMENT_CHOICE = (
-		('CREDIT','카드'),
-		('CASH','현금'),
+		('카드','카드'),
+		('현금','현금'),
+	)
+	PAYMENT_STATE = (
+		('결제','결제'),
+		('환불','환불'),
 	)
 	number = models.AutoField(primary_key=True, verbose_name="번호")	
 	paymentDate = models.DateField(default=timezone.now, verbose_name="결제일")
 	student = models.ForeignKey(Student, null=True, verbose_name="학생", on_delete=models.CASCADE)
-	paymentType = models.CharField(max_length=6, choices=PAYMENT_CHOICE, default='CREDIT', verbose_name="결제방식")
+	paymentType = models.CharField(max_length=6, choices=PAYMENT_CHOICE, default='카드', verbose_name="결제방식")
+	paymentState = models.CharField(max_length=6, choices=PAYMENT_STATE, default='결제', verbose_name="결제상태")
 	item = models.CharField(max_length=30, null=True, verbose_name="수업/상품")
 	price = models.IntegerField(default=0, verbose_name="가격")
 	note = models.CharField(max_length=100, default='', verbose_name="Memo")
