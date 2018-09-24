466ecd611d63bdc1546ee5b34552c67cea58f0f3
hangeulBae <dxdiag20@gmail.com>
2018-09-25 04:35:22 +0900

    add exit command to bat file

--- pip_install.bat
+++ pip_install.bat
@@ -1,2 +1,2 @@
 @echo off
-cmd /k "cd /d C:\Users\%USERNAME%\Desktop\privateSchoolCMS-master & pip install requirement.txt"
+cmd /k "cd /d C:\Users\%USERNAME%\Desktop\privateSchoolCMS-master & pip install requirement.txt & exit"
